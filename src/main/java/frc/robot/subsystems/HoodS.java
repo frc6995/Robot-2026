b232c215c2740719d83ac8a162613522c4d83a21
Peyton S <pey.slape@gmail.com>
2026-02-03 18:40:42 -0800

    hood logic v1 (ready to test)

--- src/main/java/frc/robot/subsystems/HoodS.java
+++ src/main/java/frc/robot/subsystems/HoodS.java
@@ -2,43 +2,37 @@ package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.DegreesPerSecond;
-import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Inches;
-import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.Second;
 import static edu.wpi.first.units.Units.Seconds;
 import static edu.wpi.first.units.Units.Volts;
 
-import java.lang.reflect.Array;
+import java.security.AllPermission;
+import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
-import edu.wpi.first.math.Pair;
-import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.AngularAcceleration;
-import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.units.measure.Mass;
-import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.util.AllianceFlipUtil;
 import frc.robot.util.POI;
-import frc.robot.util.TriggerCommand;
 import frc.robot.util.TriggerUtil;
 import yams.gearing.GearBox;
 import yams.gearing.MechanismGearing;
-import yams.mechanisms.config.ArmConfig;
 import yams.mechanisms.config.PivotConfig;
-import yams.mechanisms.positional.Arm;
 import yams.mechanisms.positional.Pivot;
 import yams.motorcontrollers.SmartMotorController;
 import yams.motorcontrollers.SmartMotorControllerConfig;
@@ -49,44 +43,40 @@ import yams.motorcontrollers.remote.TalonFXWrapper;
 
 public class HoodS extends SubsystemBase {
   public class HoodConstants {
-
-    public static final Angle kCW = Degrees.of(12.5);
-    public static final Angle kCCW = Degrees.of(40);
-
-    public static final Angle kFuelIntakeAngle = Degrees.of(0);
-    public static final Angle kStowAngle = Degrees.of(12.5);
-    public static final double[][] kAngleData = {
-        // Distance (in meters), Angle(in degrees)
-        { 1, 12.5 },
-        { 5, 40 },
-    };
-
+      // CAN IDs
+    public static final int kCANID = 42;
+      // PID-FF Constants
     public static final double kP = 0;
     public static final double kI = 0;
     public static final double kD = 0;
     public static final double kS = 0;
-    public static final double kG = 0;
-    public static final double kV = 0;
-    public static final double kA = 0;
-    public static final AngularVelocity kVelocity = DegreesPerSecond.of(0);
-    public static final AngularAcceleration kAcceleration = DegreesPerSecondPerSecond.of(0);
-    public static final double kStatorCurrentLimit = 40;
-    public static final double kSupplyCurrentLimit = 25;
-    public static final double kReduction = 40;
-    public static final boolean kMotorInverted = false;
-    public static final int kCANID = 42;
-    public static final Distance kArmLength = Inches.of(1);
-    public static final Double kMOI = 0.05;
-
+      // Sim PID-FF Constants
     public static final double kSimP = 50;
     public static final double kSimI = 0;
     public static final double kSimD = 0;
     public static final double kSimS = 0;
-    public static final double kSimG = 0;
-    public static final double kSimV = 0;
-    public static final double kSimA = 0;
-    public static final AngularVelocity kSimVelocity = DegreesPerSecond.of(90);
-    public static final AngularAcceleration kSimAcceleration = DegreesPerSecondPerSecond.of(45);
+
+      // Setpoints and Limits
+    public static final Angle kLowerLimit = Degrees.of(12.5); // CW Limit
+    public static final Angle kUpperLimit = Degrees.of(40); // CCW Limit
+    public static final Angle kStowAngle = kLowerLimit;
+    public static final double[][] kAngleData = {
+        // Distance (Meters), Angle(Degrees)
+        { 1, 12.5 },
+        { 5, 40 },
+    };
+      // Motor Setup
+    public static final double kStatorCurrentLimit = 40;
+    public static final double kSupplyCurrentLimit = 25;
+    public static final double kReduction = 40;
+    public static final boolean kMotorInverted = false;
+      // Sim Constants
+    public static final Distance kArmLength = Inches.of(1);
+    public static final Double kMOI = 0.05;
+      // Hood Safety Constants
+    public static final Distance kSafetyOverride_NoSpeed = Meters.of(1.5);
+    public static final Distance kSafetyOverride_Final = Meters.of(0.5);
+    public static final LinearVelocity kSafetyOverrideVelocity = MetersPerSecond.of(0.2);
   }
 
   public InterpolatingDoubleTreeMap table = new InterpolatingDoubleTreeMap();
@@ -94,23 +84,13 @@ public class HoodS extends SubsystemBase {
   private SmartMotorControllerConfig smcConfig = new SmartMotorControllerConfig(this)
       .withControlMode(ControlMode.CLOSED_LOOP)
       // Feedback Constants (PID Constants)
-      .withClosedLoopController(HoodConstants.kP, HoodConstants.kI, HoodConstants.kD,
-          HoodConstants.kVelocity,
-          HoodConstants.kAcceleration)
-      .withSimClosedLoopController(HoodConstants.kSimP, HoodConstants.kSimI, HoodConstants.kSimD,
-          HoodConstants.kSimVelocity,
-          HoodConstants.kSimAcceleration)
+      .withClosedLoopController(HoodConstants.kP, HoodConstants.kI, HoodConstants.kD)
+      .withSimClosedLoopController(HoodConstants.kSimP, HoodConstants.kSimI, HoodConstants.kSimD)
       // Feedforward Constants
-      .withFeedforward(new ArmFeedforward(HoodConstants.kS, HoodConstants.kG, HoodConstants.kV, HoodConstants.kA))
-      .withSimFeedforward(
-          new ArmFeedforward(HoodConstants.kSimS, HoodConstants.kSimG, HoodConstants.kSimV, HoodConstants.kSimA))
+      .withFeedforward(new SimpleMotorFeedforward(HoodConstants.kS, 0))
+      .withSimFeedforward(new SimpleMotorFeedforward(HoodConstants.kSimS, 0))
       // Telemetry name and verbosity level
       .withTelemetry("ArmMotor", TelemetryVerbosity.HIGH)
-      // Gearing from the motor rotor to final shaft.
-      // In this example GearBox.fromReductionStages(3,4) is the same as
-      // GearBox.fromStages("3:1","4:1") which corresponds to the gearbox attached to
-      // your motor.
-      // You could also use .withGearing(12) which does the same thing.
       .withGearing(new MechanismGearing(GearBox.fromReductionStages(HoodConstants.kReduction)))
       // Motor properties to prevent over currenting.
       .withMotorInverted(HoodConstants.kMotorInverted)
@@ -125,26 +105,46 @@ public class HoodS extends SubsystemBase {
 
   private PivotConfig hoodCfg = new PivotConfig(talonSmartMotorController)
       // Soft limit is applied to the SmartMotorControllers PID
-      .withSoftLimits(HoodConstants.kCW, HoodConstants.kCCW)
+      .withSoftLimits(HoodConstants.kLowerLimit, HoodConstants.kUpperLimit)
       // Hard limit is applied to the simulation.
-      .withHardLimit(HoodConstants.kCW, HoodConstants.kCCW)
+      .withHardLimit(HoodConstants.kLowerLimit, HoodConstants.kUpperLimit)
       // Starting position is where your arm starts
       .withStartingPosition(HoodConstants.kStowAngle)
       .withMOI(HoodConstants.kMOI)
-      // Length and mass of your arm for sim.
       // Telemetry name and verbosity for the arm.
       .withTelemetry("Hood", TelemetryVerbosity.HIGH);
 
   private Pivot hood = new Pivot(hoodCfg);
 
-  public HoodS() {
+  private Supplier<Pose2d> robotPose;
+  private Supplier<ChassisSpeeds> robotSpeeds;
+  private BooleanSupplier shouldApplyDynamicLimit;
+
+  public HoodS(Supplier<Pose2d> robotPose, Supplier<ChassisSpeeds> robotSpeeds) {
+    this.robotPose = robotPose;
+    this.robotSpeeds = robotSpeeds;
+
+    this.shouldApplyDynamicLimit = 
+      TriggerUtil.or(
+        TriggerUtil.and(
+          TriggerUtil.isWithinZone(
+            () -> new Translation2d(POI.kOriginToTrench.get().minus(HoodConstants.kSafetyOverride_NoSpeed), Meters.of(0)),
+            () -> new Translation2d(POI.kOriginToTrench.get().plus(HoodConstants.kSafetyOverride_NoSpeed), Meters.of(AllianceFlipUtil.FIELD_LENGTH)),
+            robotPose),
+          () -> Math.abs(robotSpeeds.get().vxMetersPerSecond) > HoodConstants.kSafetyOverrideVelocity.in(MetersPerSecond)
+        ),
+        TriggerUtil.isWithinZone(
+          () -> new Translation2d(POI.kOriginToTrench.get().minus(HoodConstants.kSafetyOverride_Final), Meters.of(0)),
+          () -> new Translation2d(POI.kOriginToTrench.get().plus(HoodConstants.kSafetyOverride_Final), Meters.of(AllianceFlipUtil.FIELD_LENGTH)),
+          robotPose));
+    
     for(double[] entry : HoodConstants.kAngleData){
       table.put(entry[0], entry[1]);
     }
   }
 
   public Command setAngle(Supplier<Angle> angle) {
-    return hood.setAngle(angle);
+    return hood.setAngle(() -> applyDynamicLimits(angle.get(), robotPose.get()));
   }
 
   public Command set(Supplier<Double> voltage) {
@@ -155,15 +155,13 @@ public class HoodS extends SubsystemBase {
     return hood.sysId(Volts.of(7), Volts.of(2).per(Second), Seconds.of(4));
   }
 
-  public Command autoHoodAngle(Supplier<Pose2d> robotPose) {
+  public Command autoHoodAngle() {
     return setAngle(
         () -> Degrees.of(table.get(robotPose.get().getTranslation().getDistance(POI.HUB1.get().getTranslation()))));
   }
 
-  public Command autoRetractHood(Supplier<ChassisSpeeds> chassisSpeeds, Supplier<Pose2d> robotPose) {
-    return Commands.either(setAngle(() -> HoodConstants.kStowAngle), autoHoodAngle(robotPose),
-        TriggerUtil.and(() -> Math.abs(chassisSpeeds.get().vxMetersPerSecond) > 0.2,  
-            TriggerUtil.isWithinZone(()->POI.LLNoHoodZone.get().getTranslation(), ()->POI.URNoHoodZone.get().getTranslation(), robotPose)));
+  public Angle applyDynamicLimits(Angle targetAngle, Pose2d robotPose) {
+    return clampAngle(targetAngle, HoodConstants.kLowerLimit, shouldApplyDynamicLimit.getAsBoolean() ? HoodConstants.kStowAngle : HoodConstants.kUpperLimit);
   }
 
   @Override
@@ -178,4 +176,14 @@ public class HoodS extends SubsystemBase {
     hood.simIterate();
   }
 
+
+  private static Angle clampAngle(Angle value, Angle low, Angle high) {
+    return Degrees.of(
+      MathUtil.clamp(
+        value.in(Degrees),
+        low.in(Degrees),
+        high.in(Degrees)
+      )
+    );
+  }
 }
--- src/main/java/frc/robot/util/POI.java
+++ src/main/java/frc/robot/util/POI.java
@@ -2,11 +2,14 @@ package frc.robot.util;
 
 import static edu.wpi.first.units.Units.Degree;
 import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Meters;
 
 import java.util.function.Supplier;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.measure.Distance;
 import frc.robot.generated.ChoreoVars;
 
 public class POI {
@@ -20,10 +23,14 @@ public class POI {
     public static final Supplier<Pose2d> BALLR1 = () -> AllianceFlipUtil.flipPose(ChoreoVars.Poses.BALLR1);
     public static final Supplier<Pose2d> TRR1 = () -> AllianceFlipUtil.flipPose(ChoreoVars.Poses.TRR1);
     public static final Supplier<Pose2d> HUB1 = () -> AllianceFlipUtil.flipPose(new Pose2d(4.625, 4.05, new Rotation2d()));
-    public static final Supplier<Pose2d> LLNoHoodZone = () -> AllianceFlipUtil.flipPose(new Pose2d(3.55, .42, new Rotation2d()));
-    public static final Supplier<Pose2d> URNoHoodZone = () -> AllianceFlipUtil.flipPose(new Pose2d(5.65, 7.64, new Rotation2d()));
     // ============= ROTATIONS =============
     public static final Supplier<Rotation2d> testEntry = () -> AllianceFlipUtil
             .flipRotation(new Rotation2d(Degrees.of(-90)));
+    // ============= TRANSLATIONS =============
+    // ============= DISTANCES =============
+    private static final Distance kOriginToTrenchBlue = Meters.of(4.6);
+    private static final Distance kOriginToTrenchRed = Meters.of(11.9);
+
+    public static final Supplier<Distance> kOriginToTrench = () -> !AllianceFlipUtil.isRedAlliance() ? kOriginToTrenchBlue : kOriginToTrenchRed;
 }
     
