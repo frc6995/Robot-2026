8cb362ee665c0a75da0fafee07e84fbd5583fae1
DJVoidd <dean.j.kennedy@icloud.com>
2026-02-06 19:17:12 -0800

    Added climb subystems + constants

--- src/main/java/frc/robot/subsystems/ClimbPivotS.java
+++ src/main/java/frc/robot/subsystems/ClimbPivotS.java
@@ -40,42 +40,7 @@ import edu.wpi.first.units.measure.Voltage;
 
 
 
-public class ClimbS extends SubsystemBase {
-    public static class ClimbConstants {
-     // PID Constants 
-    public static final double kP = 0;
-    public static final double KI = 0;
-    public static final double kD = 0; 
-     // Feedforward Constants 
-    public static final double kS = 0;
-    public static final double kV = 0;
-    public static final double kA = 0;
-     // Sim PID Constants
-    public static final double kSimP = 0;
-    public static final double kSimI = 0;
-    public static final double kSimD = 0;
-    //Sim FeedFoward Constants 
-    public static final double kSimS = 0;
-    public static final double kSimV = 0;
-    public static final double kSimA = 0;
-    // CAN IDs
-    public static final int kLeadMotorCANID = 51;
-    public static final int k1FollowMotorCANID = 52;
-    public static final int k2FollowMotorCANID = 53;
-     // Motor Config Constants 
-    public static final boolean kInvertLeadMotor = true;
-    public static final boolean kInvertFollowMotor = false;
-    public static final double kSupplyCurrentLimit = 40; 
-    public static final double kStatorCurrentLimit = 80;
-     // Sim Constants
-    public static final double kHeight = 1;
-    public static final double kMass = 100;
-     // Setpoints 
-     public static final AngularVelocity kFullExtension = RotationsPerSecond.of(4400.0/60.0);
-     public static final AngularVekocity kL1 = RotationsPerSecond.of(4400.0/60.0);
-     public static final AngularVelocity kL2 = RotationsPerSecond.of (4400/60.0);
-     public static final AngularVelocity kL3 = RotationsPerSecond.of(4400/60.0);
-     
-    }
+public class ClimbPivotS extends SubsystemBase {
+    
 
 }
---
+++ src/main/java/frc/robot/util/ClimbConstants.java
@@ -0,0 +1,76 @@
+package frc.robot.util;
+
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+
+public class ClimbConstants {
+    // PID Constants
+    public static double kP = 0;
+    public static final double KI = 0;
+    public static final double kD = 0;
+    // Feedforward Constants
+    public static final double kS = 0;
+    public static final double kV = 0;
+    public static final double kA = 0;
+    // Sim PID Constants
+    public static final double kSimP = 0;
+    public static final double kSimI = 0;
+    public static final double kSimD = 0;
+    // Sim FeedFoward Constants
+    public static final double kSimS = 0;
+    public static final double kSimV = 0;
+    public static final double kSimA = 0;
+    // CAN IDs
+    public static final int kLeadMotorCANID = 51;
+    public static final int k1FollowMotorCANID = 52;
+    public static final int k2FollowMotorCANID = 53;
+    // Motor Config Constants
+    public static final boolean kInvertLeadMotor = true;
+    public static final boolean kInvertFollowMotor = false;
+    public static final double kSupplyCurrentLimit = 40;
+    public static final double kStatorCurrentLimit = 80;
+    // Sim Constants
+    public static final double kHeight = 1;
+    public static final double kMass = 100;
+    // Setpoints
+    public static final AngularVelocity kFullExtension = RotationsPerSecond.of(4400.0 / 60.0);
+    public static final AngularVelocity kL1 = RotationsPerSecond.of(4400.0 / 60.0);
+    public static final AngularVelocity kL2 = RotationsPerSecond.of(4400 / 60.0);
+    public static final AngularVelocity kL3 = RotationsPerSecond.of(4400 / 60.0);
+
+    public record ClimbConstantsRecord(
+            double kP,
+            double kI,
+            double kD, double kS,
+            double kV,
+            double kA,
+            // Sim PID Constants
+            double kSimP,
+            double kSimI,
+            double kSimD,
+            // Sim FeedFoward Constants
+            double kSimS,
+            double kSimV,
+            double kSimA,
+            // CAN IDs
+            int kLeadMotorCANID,
+            int k1FollowMotorCANID,
+            int k2FollowMotorCANID,
+            // Motor Config Constants
+            boolean kInvertLeadMotor,
+            boolean kInvertFollowMotor,
+            double kSupplyCurrentLimit,
+            double kStatorCurrentLimit,
+            // Sim Constants
+            double kHeight,
+            double kMass,
+            // Setpoints
+            AngularVelocity kFullExtension,
+            AngularVelocity kL1,
+            AngularVelocity kL2,
+            AngularVelocity kL3) {
+
+    }
+
+}
