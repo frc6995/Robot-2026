1e9957475d56976a9e6916447793586c98db722c
Jeremy <maplecake199@gmail.com>
2026-02-17 16:57:07 -0800

    updated PID

--- src/main/java/frc/robot/subsystems/intakepivot/RealIntakePivotS.java
+++ src/main/java/frc/robot/subsystems/intakepivot/RealIntakePivotS.java
@@ -46,16 +46,16 @@ public class RealIntakePivotS extends IntakePivotS {
       // CAN IDs
     public static final int kCANID = 31;
       // Profiled PID Constants
-    public static final double kP = 56;
+    public static final double kP = 20;
     public static final double kI = 0;
-    public static final double kD = 0.2;
+    public static final double kD = 0.12;
     public static final AngularVelocity kVelocity = DegreesPerSecond.of(180);
     public static final AngularAcceleration kAcceleration = DegreesPerSecondPerSecond.of(90);
       // Feeforward Constants
     public static final double kS = 0;
-    public static final double kG = 1.210;
-    public static final double kV = 0.928;
-    public static final double kA = 0.16;
+    public static final double kG = 0.28;
+    public static final double kV = 6.83;
+    public static final double kA = 0.03;
       // Sim Profiled PID
     public static final double kSimP = 20;
     public static final double kSimI = 0;
