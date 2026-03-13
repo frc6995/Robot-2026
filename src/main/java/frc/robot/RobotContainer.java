98096b9b143b615fafd2a50733471af486c72686
Peyton S <pey.slape@gmail.com>
2026-03-12 19:32:48 -0700

    first iteration

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -89,6 +89,7 @@ public class RobotContainer {
 
     public static final CommandXboxController joystick = new CommandXboxController(0);
 
+    @Logged
     public final CommandSwerveDrivetrain m_drivetrain = new CommandSwerveDrivetrain(
             TunerConstants.DrivetrainConstants,
             TunerConstants.FrontLeft,
